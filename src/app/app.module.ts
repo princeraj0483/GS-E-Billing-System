7fa3caf2f2aa9485cfde63fe0718aa46e6df6875
munnamaical <munnamaical05@gmail.com>
2023-02-03 19:02:44 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +12,22 @@ import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { AdminSidebarComponent } from './admin/admin-sidebar/admin-sidebar.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { ShopLoginComponent } from './shop/shop-login/shop-login.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { ViewportComponent } from './viewport/viewport.component';
+import { ShopComponent } from './admin/shop/shop.component';
+import { AddEditShopComponent } from './admin/add-edit-shop/add-edit-shop.component';
+import { CustomerLoginComponent } from './customer/customer-login/customer-login.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -22,7 +37,12 @@ import { ShopLoginComponent } from './shop/shop-login/shop-login.component';
     AdminHomeComponent,
     AdminSidebarComponent,
     AdminDashboardComponent,
-    ShopLoginComponent
+    ShopLoginComponent,
+    ViewportComponent,
+    ShopComponent,
+    AddEditShopComponent,
+    CustomerLoginComponent,
+  
   ],
   imports: [
     BrowserModule,
@@ -30,7 +50,20 @@ import { ShopLoginComponent } from './shop/shop-login/shop-login.component';
     BrowserAnimationsModule,
     MatMenuModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatListModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
