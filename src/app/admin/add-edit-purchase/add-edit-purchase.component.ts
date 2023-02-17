2da76eaaadc6e8c50120343d10a6c795df15715d
munnamaical <munnamaical05@gmail.com>
2023-02-17 10:56:01 +0530

    sale design

---
+++ src/app/admin/add-edit-purchase/add-edit-purchase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-purchase',
+  templateUrl: './add-edit-purchase.component.html',
+  styleUrls: ['./add-edit-purchase.component.css']
+})
+export class AddEditPurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
